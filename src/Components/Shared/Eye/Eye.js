2b359f3b6bf2f5cc2dc73c4ebeff52b9ac27f22e
arif-hstu <arif.dvm.hstu@gmail.com>
2021-05-05 03:57:46 +0600

    eye position issue fixed

--- src/Components/Shared/Eye/Eye.js
+++ src/Components/Shared/Eye/Eye.js
@@ -12,59 +12,18 @@ function Eye() {
 		let xw;
 		let yh;
 
-		if (event.view.self.innerWidth > 1400) {
-			x = event.clientX;
-			y = event.clientY;
-			xw = (x * 100) / clienthWidth;
-			yh = (y * 200) / clientHeight;
-		}
-
-		if (event.view.self.innerWidth < 1400) {
-			x = event.clientX;
-			y = event.clientY;
-			xw = (x * 90) / clienthWidth;
-			yh = (y * 200) / clientHeight;
-
-
-			if (event.view.self.innerWidth < 1200) {
-				x = event.clientX;
-				y = event.clientY;
-				xw = (x * 120) / clienthWidth;
-				yh = (y * 200) / clientHeight;
-
-
-				if (event.view.self.innerWidth < 1000) {
-					x = event.clientX;
-					y = event.clientY;
-					xw = (x * 100) / clienthWidth;
-					yh = (y * 200) / clientHeight;
-
-
-					if (event.view.self.innerWidth < 768) {
-						x = event.clientX;
-						y = event.clientY;
-						xw = (x * 110) / clienthWidth;
-						yh = (y * 200) / clientHeight;
-					}
-				}
-			}
-		}
+		x = event.clientX;
+		y = event.clientY;
+		xw = (x * 300) / clienthWidth;
+		yh = (y * 200) / clientHeight;
 
 		let ball = document.querySelectorAll(".ball");
-		// let abs = Math.abs(50 - xw);
-		// if (abs <= 20) {
-		//   // x1 => 20=100  0=30
-		//   // x2 => 20=0   0=70
-		//   // abs*5
-		//   ball[0].style.left = xw + "%";
-		//   ball[1].style.left = xw + "%";
-		// } else {
+
 		for (var i = 0; i < 2; i++) {
-			ball[i].style.top = yh + "%";
-			ball[i].style.left = xw + "%";
-			ball[i].style.transition = "(-50%,-50%)";
+			ball[i].style.top = yh * 0.3 + 60 + "%";
+			ball[i].style.left = xw * 0.2 + 40 + "%";
+			ball[i].style.transition = "(-50%, -50%)";
 		}
-		// }
 	});
 
 
