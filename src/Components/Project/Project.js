3ee42163ffb8f5ef1cef0a74016e85eb9460a541
arif-hstu <arif.dvm.hstu@gmail.com>
2021-05-07 22:28:10 +0600

    project component improved

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -4,9 +4,47 @@ import './Project.css';
 
 function Project() {
 	const { type, id } = useParams();
+
+	const randomColor = '#3ccad1';
 	return (
 		<div className='Project'>
-			This is project
+			<div className="leftSide">
+				<div className="first">
+					first
+				</div>
+				<div className="second">
+					second
+				</div>
+				<div className="third">
+					third
+				</div>
+			</div>
+			<div className="middlePart">
+				<div className="holder">
+					holder
+				</div>
+				<div className="title">
+					title
+				</div>
+			</div>
+			<div className="rightSide">
+				<div className="first">
+					first
+				</div>
+				<div className="second">
+					second
+				</div>
+				<div className="third">
+					third
+				</div>
+			</div>
+			<div className="bottomPart">
+				<div className="backHolder">
+					<button className='primaryBtn'>
+						BACK
+					</button>
+				</div>
+			</div>
 		</div>
 	)
 }
