1b74fb8fd227e9c41a836228e1a4216a34055fbb
arif-hstu <arif.dvm.hstu@gmail.com>
2021-05-08 19:26:37 +0600

    skill added

--- src/Components/Expert/ExpertInfo/ExpertInfo.js
+++ src/Components/Expert/ExpertInfo/ExpertInfo.js
@@ -2,7 +2,19 @@ import React from 'react';
 import { motion } from 'framer-motion';
 
 import './ExpertInfo.css';
-import bar from '../../../resources/images/bar.png'
+import bar from '../../../resources/images/bar.png';
+import html from '../../../resources/icons/html.svg';
+import css from '../../../resources/icons/css.svg';
+import sass from '../../../resources/icons/sass.svg';
+import js from '../../../resources/icons/js.svg';
+import react from '../../../resources/icons/react.svg';
+import node from '../../../resources/icons/node.svg';
+
+import percent90 from '../../../resources/images/percent90.svg';
+import percent85 from '../../../resources/images/percent85.svg';
+import percent70 from '../../../resources/images/percent70.svg';
+import percent75 from '../../../resources/images/percent75.svg';
+import percent60 from '../../../resources/images/percent60.svg';
 
 function ExpertInfo() {
 
@@ -20,27 +32,51 @@ function ExpertInfo() {
 			<div className="infoHolder">
 				<div className="skills">
 					<div className="skill">
-						<h3>WEB DESIGN</h3>
+						<img src={html} alt="" />
+						<h3>HTML5</h3>
+						<img src={percent90} alt="" />
 						<p className='smallPara'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex rerum quo iusto culpa! Accusantium, laboriosam?</p>
 					</div>
 					<div className="skill">
-						<h3>GRAPHIC DESIGN</h3>
+						<img src={css} alt="" />
+						<h3>CSS3</h3>
+						<img src={percent85} alt="" />
 						<p className='smallPara'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex rerum quo iusto culpa! Accusantium, laboriosam?</p>
 					</div>
 					<div className="skill">
+					<img src={sass} alt="" />
 						<h3>
-							DEVELOPMENT
+							SASS
 					</h3>
+						<img src={percent70} alt="" />
 						<p className='smallPara'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex rerum quo iusto culpa! Accusantium, laboriosam?</p>
 					</div>
 					<div className="skill">
+					<img src={js} alt="" />
 						<h3>
-							DIGITAL MEDIA
+							JAFASCRIPT
 					</h3>
+						<img src={percent75} alt="" />
+						<p className='smallPara'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex rerum quo iusto culpa! Accusantium, laboriosam?</p>
+					</div>
+					<div className="skill">
+					<img src={react} alt="" />
+						<h3>
+							REACT
+					</h3>
+						<img src={percent85} alt="" />
+						<p className='smallPara'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex rerum quo iusto culpa! Accusantium, laboriosam?</p>
+					</div>
+					<div className="skill">
+					<img src={node} alt="" />
+						<h3>
+							NODE
+					</h3>
+						<img src={percent60} alt="" />
 						<p className='smallPara'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex rerum quo iusto culpa! Accusantium, laboriosam?</p>
 					</div>
 				</div>
-				<div className='graphHolder'>
+				<div hidden className='graphHolder'>
 					<div className="graph">
 						<p className="smallPara">
 							Specialized in UI/UX
